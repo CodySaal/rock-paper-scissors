85403de663cd54e72e753a464cd986359d646ebe
Cody <110630101+CodySaal@users.noreply.github.com>
2022-09-12 21:28:05 -0500

    Finished JS

--- game.js
+++ game.js
@@ -20,3 +20,47 @@
     // restart game
 // else 
     // end game
+
+
+    var wins = 0;
+    var ties = 0;
+    var losses = 0;
+
+    var choices = ["R", "P", "S" ]
+
+    var startGame = function() {
+        // Promts and stores human choice
+        var playerChoice = prompt("Chose R, P, or S:");
+
+        playerChoice = playerChoice.toUpperCase();
+        // Chooses a random number to use for computer choice
+        var randomNumber = Math.floor(Math.random()*choices.length);
+        // Computer chooses 
+        var computerChoice = choices[randomNumber];
+
+        alert("The computer chose " + computerChoice);
+
+        if (playerChoice === computerChoice){
+            alert("That is a tie!");
+            ties++;
+        } else if( playerChoice === "R" && computerChoice === "S" || playerChoice === "S" && computerChoice === "P" || playerChoice === "P" && computerChoice === "R"){
+            alert("You Win!!!")
+            wins++;
+        } else {
+            alert("You lose!");
+            losses++;
+        }
+
+        alert(
+            "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+        );
+
+        var playAgain =confirm("Play Again!");
+
+        if (playAgain) {
+            startGame();
+        }
+
+    }
+
+startGame();
