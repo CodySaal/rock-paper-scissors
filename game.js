c0854c70f5effca924ea1446b71b15310b4ed584
Cody <110630101+CodySaal@users.noreply.github.com>
2022-09-12 19:37:10 -0500

    pseudo-code

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+// Define array for choices [r, p, s]
+// Define var for computerChoice
+// Define var for playerChoice
+// Define var for wins
+// Define var for losses
+// Define var for ties
+
+// prompt humanChoice
+
+// randomly choose computerChoice
+
+// compare choices
+    // 
+
+// display (alert) comparison results (won, tied, lost round)
+
+// show stats (number of wins, losses, and ties)
+
+// play again?
+    // restart game
+// else 
+    // end game
